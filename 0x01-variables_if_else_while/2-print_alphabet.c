eea9810f3c42fcfe8d6d16e9d39d0d1109e5cada
Mclouis0557 <mclouismilesjr@gmail.com>
2023-10-07 00:52:43 +0000

    A program that prints the alphabet in lowercase, followed by a new line using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  *main - this is a function
+  *
+  *Return: Always 0 (success)
+  */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+
+}
