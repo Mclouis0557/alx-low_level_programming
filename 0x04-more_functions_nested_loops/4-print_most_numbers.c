6c6e16efcd8d18066d21e10de9f2bb95bbda10d2
Mclouis0557 <mclouismilesjr@gmail.com>
2023-10-14 20:46:56 +0000

    Function that prints the numbers from 0-9 excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_most_numbers -  print numbers between
+  * 0 - 9 and exclude 2 and 4.
+  *
+  * Return: no return
+  */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		if (c != 50 && c != 52)
+			_putchar(c);
+	}
+	_putchar('\n');
+}
