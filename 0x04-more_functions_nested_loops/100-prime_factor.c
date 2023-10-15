ab7a2788ee397fa6b9a36c48f567744bf45dcc54
Mclouis0557 <mclouismilesjr@gmail.com>
2023-10-15 00:19:19 +0000

    Program that finds and prints the largest factor of a large prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - prints largest prime factor
+  *
+  * Return: Always 0 (success)
+  */
+
+int main(void)
+{
+	long int i, pf;
+
+	i = 612852475143;
+	for (pf = 2; pf <= i; pf++)
+	{
+		if (i % pf == 0)
+			i /= pf;
+		pf--;
+	}
+	printf("%ld/i", pf);
+
+	return (0);
+}
