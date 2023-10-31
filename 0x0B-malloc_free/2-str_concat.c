133646361d2f97b2f9fda2fde5459809ac5d0629
Mclouis0557 <mclouismilesjr@gmail.com>
2023-10-31 21:45:11 +0000

    Function that concatenates two strings with the returned pointer pointed to a newly allocated space in memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,14 +8,15 @@
   * @s1: first string to concatenate
   * @s2: second string to concatenate
   *
-  * Return: pointer to allocated space
+  * Return: pointer to concat result, Null if failed.
   */
 
 char *str_concat(char *s1, char *s2)
 {
-	int len1, len2;
+	size_t len1, len2;
 	char *new_str;
-
+	unsigned int i, j;
+	
 	if (s1 == NULL)
 	{
 		return (s2);
@@ -27,14 +28,25 @@ char *str_concat(char *s1, char *s2)
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
-	new_str = malloc(len1 + len2 + 1 * (sizeof(char)));
+
+	new_str = malloc((len1 + len2 + 1) * (sizeof(char)));
 
 	if (new_str == NULL)
 	{
 		return (NULL);
 	}
-	memcpy(new_str, s1, len1);
-	memcpy(new_str + len1, s2, len2);
+
+	for (i = 0; i < len1; i++)
+	{
+		new_str[i] = s1[i];
+	}
+	for (j = 0; j < len2; j++)
+	{
+		new_str[len1 + j] = s2[j];
+	}
+
 	new_str[len1 + len2] = '\0';
+
 	return (new_str);
+	free(new_str);
 }
